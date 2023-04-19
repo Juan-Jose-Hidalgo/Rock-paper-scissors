5b31226bd281213f0793434c833ab5d1fc798ec2
Juan Hidalgo <juanhidalgoyanez@gmail.com>
2023-04-19 16:09:18 +0200

    feat(rules): ✨ Create rules.component
    This component is a modal with game's rules.

--- src/app/components/rules/rules.component.ts
+++ src/app/components/rules/rules.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-rules',
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./rules.component.scss']
 })
 export class RulesComponent {
+  @Input() rulesImg = '/assets/img/image-rules.svg';
+  @Output() close = new EventEmitter<boolean>();
 
+  @ViewChild('rules') rulesElement!: ElementRef;
+
+  ngAfterViewInit() {
+    this.rulesElement.nativeElement.classList.add('rules--show');
+  }
+
+  closeModal() {
+    this.changeAnimation();
+    setTimeout(() => {
+      this.close.emit(false);
+    }, 500);
+  }
+
+  changeAnimation() {
+    this.rulesElement.nativeElement.classList.add('rules--hide');
+  }
+
+  @HostListener('document:keydown.escape', ['$event'])
+  onKeyDown(event: KeyboardEvent) {
+    this.closeModal();
+  }
+
+  @HostListener('click', ['$event'])
+  onClick(event: MouseEvent) {
+    if (event.target === this.rulesElement.nativeElement) {
+      this.closeModal();
+    }
+  }
 }
